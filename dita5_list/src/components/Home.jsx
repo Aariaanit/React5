1ae7d889baca8d99a0e37a7192b2c602d44dae11
Aariaanit <arianittershnjaku@gmail.com>
2025-10-21 16:21:27 +0200

    dita5

---
+++ dita5_list/src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+// import './Home.css'
+import BlogList from './BlogList';
+
+function Home() {
+    const [list, setList] = useState([
+        {id: 1, name: "Arianit", age: 31, city: "Prizren"},
+        {id:2, name: "Arbër",age: 29,city: "Prishtinë"},
+        {id:3, name: "Ardit",age: 28,city: "Pejë"}
+
+    ]);
+
+    const deleteButton = (id) => {
+        const newList = list.filter((item) => item.id !== id);
+        setList(newList);
+    }
+
+    const viewButton = (id) => {
+        const newList = list.filter((item) => item.id === id);
+        setList(newList);
+    }
+  return (
+    <>
+        <h1>Welcome to the Home Page</h1>
+      
+            {/* {list.map((item) => (
+                <div key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p>Age: {item.age}</p>
+                    <p>City: {item.city}</p>
+                    <hr />
+                </div>
+            ))}
+             */}
+    <BlogList list={list} deleteButton={deleteButton} viewButton={viewButton} />
+    </>
+  )
+}
+export default Home
